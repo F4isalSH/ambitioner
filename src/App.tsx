8395c40d7362bf3761583c67c9e705488251ff03
F4isalSH <f4isalcode@gmail.com>
2022-01-24 11:16:18 +0300

    Added react-router-package and Creating Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import {ChakraProvider} from "@chakra-ui/react"
-
+import { ChakraProvider } from "@chakra-ui/react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Home } from "./pages/home/Home"
+import { Login } from "./pages/login/Login"
+import { Signup } from "./pages/signup/Signup"
 export const App: React.FC = () => {
   return (
     <ChakraProvider>
        <div>
-    
+         <BrowserRouter>
+         <Routes>
+           <Route path='/' element={<Home/>}/>
+           <Route path='/signup' element={<Signup/>}/>
+           <Route path='/login' element={<Login/>}/>
+           <Route path='*' element={<Home/>} />
+         </Routes>
+         </BrowserRouter>
        </div>
     </ChakraProvider>
   )
