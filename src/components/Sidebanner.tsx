c888154fb71cafe9ced42c9d3aac82ff34f11f72
F4isalSH <f4isalcode@gmail.com>
2022-01-24 12:17:36 +0300

    Added few visuals

--- src/components/Sidebanner.tsx
+++ src/components/Sidebanner.tsx
@@ -1,11 +1,17 @@
-import { VStack } from "@chakra-ui/react"
+import { VStack, Image } from "@chakra-ui/react"
 
 export const Sidebanner: React.FC = () => {
     return (
         <VStack 
         w='full'
         h='full'
-        bg='#FFB0A5'>
+        bg='#FFB0A5'
+        p={10}
+        spacing={10}
+        alignItems='flex-start'
+        >
+            <Image src='logo.svg'  />
+            <Image boxSize={500} src='illustration.svg' />
         
         </VStack>
     )
