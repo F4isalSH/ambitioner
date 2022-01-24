35b1beea27d40d24cccbe0dfde03be84dc96ec4d
F4isalSH <f4isalcode@gmail.com>
2022-01-24 21:48:01 +0300

    Added functionality to form

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -1,7 +1,18 @@
 import { Flex,Container,VStack, Text,Heading,SimpleGrid,GridItem } from "@chakra-ui/layout"
+import {useState} from 'react'
 import { Button,FormControl,FormLabel,Input } from "@chakra-ui/react"
 import { Sidebanner } from "../../components/Sidebanner"
 export const Register: React.FC = () => {
+
+    const [email,setEmail] = useState<string>('')
+    const [password,setPassword] = useState<string>('')
+    const [displayName,setDisplayName] = useState<string>('')
+
+    const handleSubmit = (e : React.FormEvent<HTMLFormElement>):void =>{
+        e.preventDefault()
+    }
+
+
     return (
         <Flex h='100vh' direction={{base: 'column', md: 'column', lg:'row'}}>
         <Sidebanner/>
@@ -9,35 +20,47 @@ export const Register: React.FC = () => {
             <VStack p={{base:19,md:25, lg:40}} alignItems='flex-start' spacing={3}>
                 <Heading>Get Started</Heading>
                 <Text color='gray.500'>Already have an account? <Button variant='link' color='#FFB0A5'>Log In</Button></Text>
+                <form onSubmit={handleSubmit}>
                 <SimpleGrid w='full' spacing={5}>
-
                 <GridItem colSpan={1}>
-                  <FormControl>
-                  <FormLabel>Email Address</FormLabel>
-                  <Input/>
+                  <FormControl isRequired>
+                  <FormLabel htmlFor='email'>Email Address</FormLabel>
+                  <Input 
+                  type='email'
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email}
+                  />
                   </FormControl>
                  </GridItem>
 
                  <GridItem colSpan={1}>
-                  <FormControl>
-                  <FormLabel>Password</FormLabel>
-                  <Input/>
+                  <FormControl isRequired>
+                  <FormLabel htmlFor='password'>Password</FormLabel>
+                  <Input 
+                  type='password'
+                  onChange={(e) => setPassword(e.target.value)}
+                  value={password}
+                  />
                   </FormControl>
                  </GridItem>
 
                  <GridItem colSpan={1}>
-                  <FormControl>
+                  <FormControl isRequired>
                   <FormLabel>Display Name</FormLabel>
-                  <Input placeholder="John"/>
+                  <Input 
+                  placeholder="Faisal"
+                  onChange={(e) => setDisplayName(e.target.value)}
+                  value={displayName}
+                  />
                   </FormControl>
                  </GridItem>
 
                  <GridItem  colSpan={1}>
-                 <Button bg='#FFB0A5' color='white' size="lg" w="full">Register</Button>
+                 <Button type='submit' bg='#FFB0A5' color='white' size="lg" w="full">Register</Button>
                  </GridItem>
+                 </SimpleGrid>
+                </form>
                     
-                </SimpleGrid>
-
             </VStack>
         </Container>
         </Flex>
