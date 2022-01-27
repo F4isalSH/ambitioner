ba3a63223a4fdb7b91c73b648b068acd1fc89027
F4isalSH <f4isalcode@gmail.com>
2022-01-27 20:29:36 +0300

    Initializing firebase

---
+++ src/firebase/config.ts
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAmTiHWNn0DSAUYna-0vepwzVRmHG7erzc",
+    authDomain: "ambitioner-3d73f.firebaseapp.com",
+    projectId: "ambitioner-3d73f",
+    storageBucket: "ambitioner-3d73f.appspot.com",
+    messagingSenderId: "594670577795",
+    appId: "1:594670577795:web:d608b46e03e735f5dd53b6"
+  };
+
+  //init firebase
+  firebase.initializeApp(firebaseConfig)
+
+  const projectFirestore = firebase.firestore()
+
+  export {projectFirestore}
