437e582a0631ee6c20b8123ffff413bfd31973c7
F4isalSH <f4isalcode@gmail.com>
2022-01-28 21:03:11 +0300

    Created initial sign up hook

---
+++ src/hooks/useSignup.ts
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import { projectAuth } from "../firebase/config";
+
+export const useSignup = () =>{
+    const [error, setError] = useState(null)
+    const [isPending, setIsPending] = useState<boolean>(false)
+
+
+}
