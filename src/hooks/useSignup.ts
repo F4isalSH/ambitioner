0e87c51c2838c5e814a878fa0ceaa481c3a38d43
F4isalSH <f4isalcode@gmail.com>
2022-01-29 12:44:57 +0300

    Created logic for useSignup hook

--- src/hooks/useSignup.ts
+++ src/hooks/useSignup.ts
@@ -1,9 +1,44 @@
 import { useState } from "react";
 import { projectAuth } from "../firebase/config";
 
+interface signUpCredentials {
+    email:string
+    password:string
+    displayName:string
+}
+
 export const useSignup = () =>{
-    const [error, setError] = useState(null)
+    const [error, setError] = useState<any>(null)
     const [isPending, setIsPending] = useState<boolean>(false)
 
+    const signup = async (credentials:signUpCredentials)=> {
+        setError(null)
+        setIsPending(true)
 
+        try{
+
+           const res = await projectAuth.createUserWithEmailAndPassword(credentials.email, credentials.password)
+           console.log(res.user)
+
+           if(!res){
+               throw new Error ('Could not complete signup')
+           }
+
+           await res.user?.updateProfile({displayName: credentials.displayName})
+
+           setIsPending(false)
+           setError(null)
+
+        }
+        catch(err){
+            console.log(err.message)
+            setError(err.message)
+            setIsPending(false)
+        }
+
+    }
+    
+    return{error, isPending, signup}
 }
+
+
