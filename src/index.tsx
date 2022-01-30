4dfa346adf071f28eb6c7b6bbc1b945c1ee5366c
F4isalSH <f4isalcode@gmail.com>
2022-01-30 19:43:47 +0300

    Wrapping app with AuthContextProvider

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,13 @@
 import * as React from "react"
 import ReactDOM from "react-dom"
 import { App } from "./App"
+import {AuthContextProvider} from './context/AuthContext'
+
 ReactDOM.render(
   <React.StrictMode>
+    <AuthContextProvider>
     <App />
+    </AuthContextProvider>
   </React.StrictMode>,
   document.getElementById("root"),
 )
