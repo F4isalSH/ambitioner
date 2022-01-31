d00c8cdd101272cc4d62cfa969648991b3e57731
F4isalSH <f4isalcode@gmail.com>
2022-01-31 09:05:40 +0300

    Using login hook in login page

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,59 +1,87 @@
-import { Flex,VStack, Text,Heading,SimpleGrid,GridItem, Center} from "@chakra-ui/layout"
-import {useState} from 'react'
-import { Button,FormControl,FormLabel,Input } from "@chakra-ui/react"
-import { Sidebanner } from "../../components/Sidebanner"
-import { useNavigate } from "react-router"
+import {
+  Flex,
+  VStack,
+  Text,
+  Heading,
+  SimpleGrid,
+  GridItem,
+  Center,
+} from "@chakra-ui/layout";
+import { useState } from "react";
+import { Button, FormControl, FormLabel, Input } from "@chakra-ui/react";
+import { Sidebanner } from "../../components/Sidebanner";
+import { useNavigate } from "react-router";
+import { useLogin } from "../../hooks/useLogin";
 
 export const Login: React.FC = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const { error, isPending, login } = useLogin();
 
-    const navigate = useNavigate();
-    const [email,setEmail] = useState<string>('')
-    const [password,setPassword] = useState<string>('')
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    login(email, password);
+  };
 
-    const handleSubmit = (e : React.FormEvent<HTMLFormElement>):void =>{
-        e.preventDefault()
-    }
-
-
-    return (
-        <Flex h='100vh' direction={{base: 'column', md: 'column', lg:'row'}}>
-        <Sidebanner/>
-        <Center  w='full' height='full'>
-            <VStack py={5} alignItems='flex-start' spacing={3}>
-                <Heading>Welcome</Heading>
-                <Text color='gray.500'>Don't have an account? <Button onClick={()=>navigate('/register')} variant='link' color='#EB615A'>Register</Button></Text>
-                <form onSubmit={handleSubmit}>
-                <SimpleGrid w='full' spacing={5}>
-                <GridItem colSpan={1}>
-                  <FormControl isRequired>
-                  <FormLabel htmlFor='email'>Email Address</FormLabel>
-                  <Input 
-                  type='email'
-                  onChange={(e) => setEmail(e.target.value)}
-                  value={email}
+  return (
+    <Flex h="100vh" direction={{ base: "column", md: "column", lg: "row" }}>
+      <Sidebanner />
+      <Center w="full" height="full">
+        <VStack py={5} alignItems="flex-start" spacing={3}>
+          <Heading>Welcome</Heading>
+          <Text color="gray.500">
+            Don't have an account?{" "}
+            <Button
+              onClick={() => navigate("/register")}
+              variant="link"
+              color="#EB615A"
+            >
+              Register
+            </Button>
+          </Text>
+          <form onSubmit={handleSubmit}>
+            <SimpleGrid w="full" spacing={5}>
+              <GridItem colSpan={1}>
+                <FormControl isRequired>
+                  <FormLabel htmlFor="email">Email Address</FormLabel>
+                  <Input
+                    type="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
                   />
-                  </FormControl>
-                 </GridItem>
+                </FormControl>
+              </GridItem>
 
-                 <GridItem colSpan={1}>
-                  <FormControl isRequired>
-                  <FormLabel htmlFor='password'>Password</FormLabel>
-                  <Input 
-                  type='password'
-                  onChange={(e) => setPassword(e.target.value)}
-                  value={password}
+              <GridItem colSpan={1}>
+                <FormControl isRequired>
+                  <FormLabel htmlFor="password">Password</FormLabel>
+                  <Input
+                    type="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
                   />
-                  </FormControl>
-                 </GridItem>
+                </FormControl>
+              </GridItem>
 
-                 <GridItem  colSpan={1}>
-                 <Button type='submit' bg='#EB615A' _hover={{bg:'#FFB0A5'}} color='white' size="lg" w="full">Login</Button>
-                 </GridItem>
-                 </SimpleGrid>
-                </form>
-                    
-            </VStack>
-        </Center>
-        </Flex>
-    )
-}
+              <GridItem colSpan={1}>
+                <Button
+                  type="submit"
+                  isLoading={isPending}
+                  bg="#EB615A"
+                  _hover={{ bg: "#FFB0A5" }}
+                  color="white"
+                  size="lg"
+                  w="full"
+                >
+                  Login
+                </Button>
+                {error && <p>{error}</p>}
+              </GridItem>
+            </SimpleGrid>
+          </form>
+        </VStack>
+      </Center>
+    </Flex>
+  );
+};
