1b8865292b75e011ec0c4a7c3bccb065160dc529
F4isalSH <f4isalcode@gmail.com>
2022-01-31 12:41:22 +0300

    Adding Menu toggle in Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,11 @@
+import { useState } from "react";
+import { Container, Image } from "@chakra-ui/react";
+import { MenuToggle } from "./MenuToggle";
 export const Navbar: React.FC = () => {
-  return <div></div>;
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return <MenuToggle toggleMenu={toggleMenu} isOpen={isOpen} />;
 };
