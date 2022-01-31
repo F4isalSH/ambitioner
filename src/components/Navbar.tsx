b94414336e03ff28e756ac44827118307d1fc3cb
F4isalSH <f4isalcode@gmail.com>
2022-01-31 16:43:04 +0300

    Changed hover color

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,13 @@ export const Navbar: React.FC = () => {
     <HStack w="full" py="10" px="20" justifyContent="space-between">
       <Image src="logo.svg" boxSize={{ base: 150 }} />
       <HStack spacing="7">
-        <Button bg="#EB615A" color="white" borderRadius="full" boxShadow="base">
+        <Button
+          bg="#EB615A"
+          color="white"
+          borderRadius="full"
+          boxShadow="base"
+          _hover={{ bg: "#FFB0A5" }}
+        >
           <Icon as={CgDarkMode} />
         </Button>
         <Button
@@ -15,6 +21,7 @@ export const Navbar: React.FC = () => {
           color="white"
           borderRadius="full"
           boxShadow="base"
+          _hover={{ bg: "#FFB0A5" }}
           onClick={logout}
         >
           Logout
