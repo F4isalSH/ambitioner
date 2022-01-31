1bd643f679563f924b6c9c0619aff68df1476b7b
F4isalSH <f4isalcode@gmail.com>
2022-01-31 13:57:20 +0300

    Setting up flex properties

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,7 +2,11 @@ import { Dashboard } from "../../components/Dashboard";
 import { Flex } from "@chakra-ui/react";
 export const Home: React.FC = () => {
   return (
-    <Flex>
+    <Flex
+      h="100vh"
+      bg="#F3F8FB"
+      direction={{ base: "column", md: "column", lg: "row" }}
+    >
       <Dashboard />
     </Flex>
   );
