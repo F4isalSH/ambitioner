a37924a1c943f6a25dd768cda80432d9c5af78cb
F4isalSH <f4isalcode@gmail.com>
2022-01-31 13:40:53 +0300

    Refactor

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,18 +3,16 @@ import { CgDarkMode } from "react-icons/cg";
 import { HStack, Image, Button, Icon } from "@chakra-ui/react";
 export const Navbar: React.FC = () => {
   return (
-    <nav>
-      <HStack w="full" py="10" px="20" justifyContent="space-between">
-        <Image src="logo.svg" boxSize={{ base: 150 }} />
-        <HStack>
-          <Button bg="#EB615A" color="white" borderRadius="full">
-            <Icon as={CgDarkMode} />
-          </Button>
-          <Button bg="#EB615A" color="white" borderRadius="full">
-            Logout
-          </Button>
-        </HStack>
+    <HStack w="full" py="10" px="20" justifyContent="space-between">
+      <Image src="logo.svg" boxSize={{ base: 150 }} />
+      <HStack>
+        <Button bg="#EB615A" color="white" borderRadius="full">
+          <Icon as={CgDarkMode} />
+        </Button>
+        <Button bg="#EB615A" color="white" borderRadius="full">
+          Logout
+        </Button>
       </HStack>
-    </nav>
+    </HStack>
   );
 };
