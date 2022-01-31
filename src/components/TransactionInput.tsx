70de5f035eb13e7f9bd955977c53a8b870f2bf79
F4isalSH <f4isalcode@gmail.com>
2022-01-31 14:09:39 +0300

    Styling Transactions VStack

--- src/components/TransactionInput.tsx
+++ src/components/TransactionInput.tsx
@@ -1,5 +1,5 @@
 import { VStack, HStack } from "@chakra-ui/layout";
 
 export const TransactionInput: React.FC = () => {
-  return <VStack bg="white" w="50%"></VStack>;
+  return <VStack borderRadius="xl" boxShadow="lg" bg="white" w="50%"></VStack>;
 };
