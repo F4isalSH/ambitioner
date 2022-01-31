20a8b0b5a506a4251b8592aef1946857ad6ea180
F4isalSH <f4isalcode@gmail.com>
2022-01-31 13:39:41 +0300

    Creating navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,20 @@
 import { useState } from "react";
-import { Container, Image } from "@chakra-ui/react";
-import { MenuToggle } from "./MenuToggle";
+import { CgDarkMode } from "react-icons/cg";
+import { HStack, Image, Button, Icon } from "@chakra-ui/react";
 export const Navbar: React.FC = () => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
-
-  return <MenuToggle toggleMenu={toggleMenu} isOpen={isOpen} />;
+  return (
+    <nav>
+      <HStack w="full" py="10" px="20" justifyContent="space-between">
+        <Image src="logo.svg" boxSize={{ base: 150 }} />
+        <HStack>
+          <Button bg="#EB615A" color="white" borderRadius="full">
+            <Icon as={CgDarkMode} />
+          </Button>
+          <Button bg="#EB615A" color="white" borderRadius="full">
+            Logout
+          </Button>
+        </HStack>
+      </HStack>
+    </nav>
+  );
 };
