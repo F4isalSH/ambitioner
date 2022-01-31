790406dc4f004b0d1044517edc5b8cb591f86e87
F4isalSH <f4isalcode@gmail.com>
2022-01-31 10:16:12 +0300

    Added Route Guarding

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,32 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Home } from "./pages/home/Home";
 import { Login } from "./pages/login/Login";
 import { Register } from "./pages/register/Register";
 import { useAuthContext } from "./hooks/useAuthContext";
+
 export const App: React.FC = () => {
-  const { authIsReady } = useAuthContext();
+  const { user, authIsReady } = useAuthContext();
   return (
     <div>
       {authIsReady && (
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="*" element={<Home />} />
+            <Route
+              path="/"
+              element={user ? <Home /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/register"
+              element={user ? <Navigate to="/" /> : <Register />}
+            />
+            <Route
+              path="/login"
+              element={user ? <Navigate to="/" /> : <Login />}
+            />
+            <Route
+              path="*"
+              element={user ? <Navigate to="/" /> : <Navigate to="/login" />}
+            />
           </Routes>
         </BrowserRouter>
       )}
