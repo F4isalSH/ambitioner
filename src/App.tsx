a299ba80fcec90e680a26d8fae49965bb3846c5a
F4isalSH <f4isalcode@gmail.com>
2022-01-31 09:41:39 +0300

    Added authentication check: only renders when authentication is ready

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/home/Home";
 import { Login } from "./pages/login/Login";
 import { Register } from "./pages/register/Register";
+import { useAuthContext } from "./hooks/useAuthContext";
 export const App: React.FC = () => {
+  const { authIsReady } = useAuthContext();
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="*" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+      {authIsReady && (
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      )}
     </div>
   );
 };
