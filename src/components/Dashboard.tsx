802aac728fc9710a9c60b312f3802e544acaee73
F4isalSH <f4isalcode@gmail.com>
2022-01-31 17:26:51 +0300

    Aligning heading

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,12 +1,13 @@
 import { Navbar } from "./Navbar";
-import { VStack, Heading, useColorModeValue } from "@chakra-ui/react";
+import { VStack, Heading } from "@chakra-ui/react";
 import { useAuthContext } from "../hooks/useAuthContext";
+
 export const Dashboard: React.FC = ({}) => {
   const { user }: any = useAuthContext();
   return (
     <VStack bg="#F3F8FB" alignItems="flex-start" w="full" h="100vh">
       <Navbar />
-      <Heading px="20" fontFamily="mono" size="md">
+      <Heading px="40" fontFamily="Poppins">
         Welcome back, {user.displayName}.
       </Heading>
     </VStack>
