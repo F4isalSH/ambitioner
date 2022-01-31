3b63faa50b36fa306fe1b598ce6ecfc102e04eda
F4isalSH <f4isalcode@gmail.com>
2022-01-31 17:52:32 +0300

    Adding icon

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,15 +1,29 @@
 import { Navbar } from "./Navbar";
-import { VStack, Heading } from "@chakra-ui/react";
+import { VStack, Heading, HStack, Icon } from "@chakra-ui/react";
 import { useAuthContext } from "../hooks/useAuthContext";
+import { FaMoneyBillWave } from "react-icons/fa";
 
 export const Dashboard: React.FC = ({}) => {
   const { user }: any = useAuthContext();
   return (
-    <VStack bg="#F3F8FB" alignItems="flex-start" w="full" h="100vh">
+    <VStack
+      spacing={10}
+      bg="#F3F8FB"
+      alignItems="flex-start"
+      w="full"
+      h="100vh"
+    >
       <Navbar />
-      <Heading px="40" fontFamily="Poppins">
+      <Heading px="40" fontFamily="mono" size="lg">
         Welcome back, {user.displayName}.
       </Heading>
+
+      <HStack px={40}>
+        <Heading fontFamily="mono" size="md">
+          Transactions
+        </Heading>
+        <Icon boxSize={6} as={FaMoneyBillWave} color="#EB615A" />
+      </HStack>
     </VStack>
   );
 };
