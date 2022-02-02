e2c05656e744bc335548c74bb82c0981a284bc15
F4isalSH <f4isalcode@gmail.com>
2022-02-02 09:52:32 +0300

    Created logic for useCollection

--- src/hooks/useCollection.ts
+++ src/hooks/useCollection.ts
@@ -1,4 +1,33 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import { projectFirestore } from "../firebase/config";
 
-export const useCollection = (collection: string) => {};
+export const useCollection = (collection: any) => {
+  const [documents, setDocuments] = useState(null);
+  const [error, setError] = useState<any>(null);
+
+  useEffect(() => {
+    let ref = projectFirestore.collection(collection);
+
+    const unsubscribe = ref.onSnapshot(
+      (snapshot) => {
+        let results: any = [];
+        snapshot.docs.forEach((doc) => {
+          results.push({ ...doc.data(), id: doc.id });
+        });
+
+        //updating the state
+        setDocuments(results);
+        setError(null);
+      },
+      (error: any) => {
+        console.log(error);
+        setError("Could not fetch data");
+      }
+    );
+
+    //unsubscribe
+    return () => unsubscribe();
+  }, [collection]);
+
+  return { documents, error };
+};
