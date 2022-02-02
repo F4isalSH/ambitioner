d83bd4ab06b86c9801863c2db12a69128a66f0b6
F4isalSH <f4isalcode@gmail.com>
2022-02-02 11:29:17 +0300

    Setup transaction layout

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,5 +1,5 @@
 import { Navbar } from "./Navbar";
-import { VStack, Heading, HStack, Icon } from "@chakra-ui/react";
+import { VStack, Heading, HStack, Icon, Center } from "@chakra-ui/react";
 import { useAuthContext } from "../hooks/useAuthContext";
 import { FaMoneyBillWave } from "react-icons/fa";
 import { TransactionList } from "./TransactionList";
@@ -15,16 +15,18 @@ export const Dashboard: React.FC = ({}) => {
       h="100vh"
     >
       <Navbar />
-      <Heading px="40" fontFamily="mono" size="lg">
-        Welcome back, {user.displayName}.
-      </Heading>
 
-      <HStack px={40}>
-        <Heading fontFamily="mono" size="md">
-          Transactions
+      <Center w="full" flexDirection="column">
+        <Heading fontFamily="mono" size="lg">
+          Welcome back, {user.displayName}.
         </Heading>
-        <Icon boxSize={6} as={FaMoneyBillWave} color="#EB615A" />
-      </HStack>
+        <HStack pt={10}>
+          <Heading fontFamily="mono" size="md">
+            Transactions
+          </Heading>
+          <Icon boxSize={6} as={FaMoneyBillWave} color="#EB615A" />
+        </HStack>
+      </Center>
       <TransactionList uid={user.uid} />
     </VStack>
   );
